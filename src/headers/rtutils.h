e93a02625773357ad69f42f0bd59365817c17724
Giovanni [dacav] Simoni <simgidacav@gmail.com>
2010-07-14 14:17:48 +0200

    bugfix

---
+++ src/headers/rtutils.h
@@ -0,0 +1,47 @@
+#ifndef __defined_headers_rtutils_h
+#define __defined_headers_rtutils_h
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <time.h>
+#include <assert.h>
+
+#define SECOND_NS 1000000000UL
+
+static inline
+void rtutils_get_now (struct timespec *n)
+{
+    clock_gettime(CLOCK_MONOTONIC, n);
+}
+
+static inline
+void rtutils_wait (const struct timespec *delay,
+                    struct timespec *remain)
+{
+    assert(clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME,
+                           delay, remain) == 0);
+}
+
+/* Make absolute a certain time wait by adding some 'current time' */
+void rtutils_time_increment (struct timespec *target,
+                             const struct timespec *val);
+
+/* Comparsion between timespec structures, allows to determine which
+ * period is greater
+ */
+int rtutils_time_cmp (const struct timespec *s0,
+                      const struct timespec *s1);
+
+static inline
+int rtutils_time_iszero (const struct timespec *s)
+{
+    return s->tv_sec == 0 && s->tv_nsec == 0;
+}
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif // __defined_headers_rtutils_h
+
---
+++ src/rtutils.c
@@ -0,0 +1,25 @@
+#include "headers/rtutils.h"
+
+void rtutils_time_increment (struct timespec *target,
+                             const struct timespec *val)
+{
+    long nano;
+
+    target->tv_sec += val->tv_sec;
+    nano = target->tv_nsec + val->tv_nsec;
+    if (nano > SECOND_NS) {
+        target->tv_sec ++;
+        nano -= SECOND_NS;
+    }
+    target->tv_nsec = nano;
+}
+
+int rtutils_time_cmp (const struct timespec *s0,
+                      const struct timespec *s1)
+{
+    if (s0->tv_sec == s1->tv_sec)
+        return s0->tv_nsec > s1->tv_nsec ? -1 : 1;
+    return s0->tv_sec > s1->tv_sec ? -1 : 1;
+}
+
+
